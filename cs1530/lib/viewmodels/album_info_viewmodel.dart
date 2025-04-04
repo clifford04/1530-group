70ef034828ed00827b6a28a2e5b4ce82eb3d6446
Connor Clifford <114612739+clifford04@users.noreply.github.com>
2025-04-04 11:28:21 -0400

    added detials about rating

---
+++ cs1530/lib/viewmodels/album_info_viewmodel.dart
@@ -0,0 +1,17 @@
+// contains code for fetching a album's details
+
+// CONNOR
+
+import 'package:flutter/material.dart';
+import 'package:mvvm_flutter/web_service.dart';
+import 'package:mvvm_flutter/viewmodels/album_details_view_model.dart';
+
+class AlbumInfoViewModel extends ChangeNotifier {
+  AlbumDetailsViewModel? albumDetails;
+
+  Future<void> fetchAlbumDetails(String iD) async {
+    final results = await Webservice().fetchAlbumDetails(iD);
+    albumDetails = AlbumDetailsViewModel(albumDetails: results);
+    notifyListeners();
+  }
+}
---
+++ cs1530/lib/viewmodels/rating_view_model.dart
@@ -0,0 +1,23 @@
+// when a user rates a album, this allows access to the value
+// associated with the rating (aka number of stars)
+
+// RAGUL
+
+import 'package:flutter/material.dart';
+
+// RatingProvider to manage the rating globally
+class RatingProvider extends ChangeNotifier {
+  int _rating = 0;
+
+  int get rating => _rating;
+
+  // when rating a song, value is initially 0 stars
+  init() {
+    _rating = 0;
+  }
+
+  void updateRating(int newRating) {
+    _rating = newRating;
+    notifyListeners();
+  }
+}
