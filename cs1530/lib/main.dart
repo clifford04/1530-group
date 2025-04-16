915165b2c55f849a3e7a65122a81022253e7b875
oln10 <oln10@pitt.edu>
2025-04-16 13:44:00 -0400

    working navigation

---
+++ cs1530/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+import 'package:mvvm_flutter/views/friend_page.dart';
+import 'package:mvvm_flutter/views/chats_page.dart';
+import 'package:mvvm_flutter/views/review_page.dart';
+
+void main() {
+    runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'MusicBoxd',
+        theme: ThemeData(primarySwatch: Colors.blue, canvasColor: Colors.red),
+        home: MainPage());
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  // review page is first page that user is taken to when app runs
+  int _currentIndex = 2;
+
+  final List<String> _routes = [
+    '/friend',
+    '/chat',
+    '/review',
+  ];
+
+  // A GlobalKey for the Navigator
+  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
+
+  void _onTap(int index) {
+    setState(() {
+      _currentIndex = index;
+      _navigatorKey.currentState!.pushNamedAndRemoveUntil(
+        _routes[index],
+        (route) => false,
+      );
+    });
+  }
+
+  // each route takes user to different page
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Navigator(
+        key: _navigatorKey,
+        initialRoute: '/review',
+        onGenerateRoute: (settings) {
+          switch (settings.name) {
+            case '/friend':
+              return MaterialPageRoute(builder: (context) => FriendPage());
+            case '/review':
+              return MaterialPageRoute(builder: (context) => ReviewPage());
+            case '/chat':
+              return MaterialPageRoute(builder: (context) => ChatScreen());
+          }
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onTap,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Friends',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.textsms),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Review',
+          ),
+        ],
+      ),
+    );
+  }
+}
